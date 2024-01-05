ab32724658126de131eaffe728257f59ed7bb9e9
Emma Gerigscott <elgerig@gmail.com>
2024-01-05 14:19:35 -0800

    Refactor prompt.js and add prompt.mjs

---
+++ functions/prompt.mjs
@@ -0,0 +1,40 @@
+import { createPrompt } from "../promptGenerator.js";
+
+const updatePrompt = async () => {
+  let prompt;
+  try {
+    prompt = await createPrompt();
+
+  } catch (err) {
+    prompt = err.message;
+  }
+  return prompt;
+};
+
+export default async (req) => {
+  const newPrompt = await updatePrompt();
+  const apiUrl = process.env.PROMPT_API_URL;
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ promptText: newPrompt }),
+  });
+  if (response.ok) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ promptText: newPrompt }),
+    };
+  } else {
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+}
+
+export const config = {
+  schedule: "@daily"
+}
